435a4476acc63d1477a827ea062ae85a696d8890
DomiUmami <williams_dominik@yahoo.com>
2024-07-07 20:20:12 -0400

    wip

--- index.js
+++ index.js
@@ -117,7 +117,8 @@ class Car {
   constructor(name, tankSize, mpg) {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -134,7 +135,15 @@ class Car {
    * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+    const milesCanDrive = this.tank * this.mpg
+    if (distance <= milesCanDrive){
+      this.odometer = this.odometer + distance
+      this.tank = this.tank - (distance / this.mpg)
+    } else {
+      this.tank = 0
+    this.odometer = this.odometer + milesCanDrive
+  }
+  return this.odometer
   }
 
   /**
@@ -149,9 +158,15 @@ class Car {
    * focus.refuel(99) // returns 600 (tank only holds 20)
    */
   refuel(gallons) {
-    // ✨ implement
+    if (gallons <= this.tankSize - this.tank){
+      this.tank = this.tank + gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
+
 
 /**
  * [Exercise 7] Asynchronously resolves whether a number is even
